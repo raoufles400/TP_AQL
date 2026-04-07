ed5bfa5d8325126cb06cdc23c2a8e59f05580ef1
raoufles400 <raoufmennai400@gmail.com>
2026-04-07 11:09:22 +0200

    Structure de L'application de exo1_tp2

---
+++ tp2/src/main/java/Exo1/UserRepository.java
@@ -0,0 +1,5 @@
+package com.tp2.Exo1;
+
+public interface UserRepository {
+    User findUserById(long id);
+}
---
+++ tp2/src/main/java/tpaql/App.java
@@ -0,0 +1,12 @@
+package tpaql;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
